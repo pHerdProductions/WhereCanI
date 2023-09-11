8fe20c0c0de4e8309c2eff6a7d185ceccaf74c9f
Chiemeka Anunkor <chiemeka.anunkor@perficient.com>
2023-09-11 11:16:18 -0500

    added backend folder structure, added models for users, create user endpoint

--- client/app/index.js
+++ client/app/index.js
@@ -1,4 +1,4 @@
-import { View,StyleSheet,Text } from 'react-native';
+import { View,StyleSheet,Text,Header } from 'react-native';
 import { Link } from 'expo-router';
 
 export default function Page() {
