4882ddde19f928590c5ce9e045b855276782af4f
pHerdProductions <pherdproductions@gmail.com>
2023-09-23 15:34:43 -0400

    react-native-dotenv for .env usage

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,7 +1,20 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['expo-router/babel'],
+    presets: [
+      'babel-preset-expo',
+      'module:metro-react-native-babel-preset'
+    ],
+    plugins: [
+      'expo-router/babel',
+      ["module:react-native-dotenv", {
+        "envName": "APP_ENV",
+        "moduleName": "@env",
+        "path": ".env",
+        "safe": false,
+        "allowUndefined": true,
+        "verbose": false
+      }]
+    ],
   };
 };
