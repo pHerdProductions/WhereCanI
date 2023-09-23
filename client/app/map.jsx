d54e5aabe6907c9e86244dbb4b8efb3c7667b30d
pHerdProductions <pherdproductions@gmail.com>
2023-09-23 15:29:39 -0400

    accepts params to set the maps region

--- client/app/map.jsx
+++ client/app/map.jsx
@@ -1,57 +1,42 @@
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Image, StyleSheet, Text, View, Dimensions,SafeAreaView,Button } from 'react-native';
+import { Image, StyleSheet, Text, View, Dimensions, SafeAreaView, Button } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
-const map=()=>{
+export default MapPage = ({ route, navigation }) => {
+
+    const deviceWidth = Dimensions.get('screen').width;
+    const deviceHeight = Dimensions.get('screen').height;
+    const deviceRatio = deviceWidth / deviceHeight; // Device screen's aspect ratio
+
+    const { lat, lng, latDelta } = route.params;
 
     const [region, setRegion] = useState({
-        latitude: 32.668870,
-        longitude: -79.907630,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01
+        latitude: lat,
+        longitude: lng,
+        latitudeDelta: latDelta,
+        longitudeDelta: latDelta * deviceRatio,
       });
     
+      // Sample region Type -- of folly beach
       const regionFollyBeach = {
         latitude: 32.667870679074494,
         longitude: -79.90863058239091,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
       };
-    
-      const dw = Dimensions.get('screen').width;
-      const dh = Dimensions.get('screen').height;
-    
 
     return (
-
        <SafeAreaView style={styles.safeAreaView}>
-        <View style={styles.nav}>
-        <Link style={styles.button} href="/home">home</Link>
-        <Text>map</Text>
-       
-      <Button
-        title="Press me"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
-
-        </View>
-    
         <View style={styles.container}>
         
         {/*Render our MapView*/}
           <MapView
             style={styles.map}
-            // Specify our initial coordinates/region
-            initialRegion = {{
-              latitude: 32.668870679074494,
-              longitude: -79.90763058239091,
-              latitudeDelta: 0.01,
-              longitudeDelta: 0.01,
-            }}
-    
-            // When the user stops scrolling/panning, change the current coordinates: 
+            initialRegion={region} // Specify our initial coordinates/region
+
+            // When the user stops scrolling/panning, set the region: 
             onRegionChangeComplete = {(region) => setRegion(region)}
           >
     
@@ -73,13 +58,14 @@ const map=()=>{
           }}/>
     
           <StatusBar style='auto' />
-          <Text style={styles.text}>W: {dw} , H: {dh}</Text>
           <Text style={styles.text}>Current LAT: {region.latitude}</Text>
           <Text style={styles.text}>Current LON: {region.longitude}</Text>
-        </View>
-        </SafeAreaView>
+          <Text style={styles.text}>Current LATD: {region.latitudeDelta}</Text>
+          <Text style={styles.text}>Current LNGD: {region.longitudeDelta}</Text>
 
-      );
+        </View>
+      </SafeAreaView>
+    );
 
 }
 
@@ -132,7 +118,3 @@ const styles = StyleSheet.create({
         
       },
   });
-
-
-
-export default map
