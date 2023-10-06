5a38be868aadf3a3f9fd91c7e0d098c675dc2bb8
Ralph Sabatino <pHerdProductions@gmail.com>
2023-10-06 11:58:21 -0400

    added search route & changed to es6

--- server/router/index.js
+++ server/router/index.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
 
-const { createPOI, getAllPOI, getIndividualPOI } = require('../controller/poiController');
-const { createUser, getAllUser, loginUser } = require('../controller/userController');
-const { createPost, getAllPost } = require('../controller/postController');
+import { createPOI, getAllPOI, getIndividualPOI, searchPOI } from '../controller/poiController';
+import { createUser, getAllUser, loginUser } from '../controller/userController';
+import { createPost, getAllPost } from '../controller/postController';
 
 router.route('/signup').post(createUser);
 router.route('/login').post(loginUser);
 router.route('/poi').get(getAllPOI).post(createPOI);
+router.route('/search').get(searchPOI);
 //router.route("/poi/id").get(getIndividualPOI)
 
 router.route('/post').get(getAllPost).post(createPost);
 
-module.exports=router;
+export { router };
