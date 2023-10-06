219453bc74a1c035283ec93d3851d644ecb3c228
Ralph Sabatino <pHerdProductions@gmail.com>
2023-10-06 09:36:29 -0400

    Added searchPOI & cleaned

--- server/controller/poiController.js
+++ server/controller/poiController.js
@@ -1,47 +1,63 @@
-const prisma= require('../db/prisma')
+const prisma = require('../db/prisma');
 
 module.exports = {
-    createPOI: async (req, res) => {
-        try {
-          let hashtag = req.body.hashtags.split(",")
-          req.body.hashtags=hashtag
-          let newPoi=req.body
 
-          await prisma.poi.create({
-            data: newPoi
-          })
-      res.status(200).json({Message:"Success a new POI was created",data:newPoi})
-        } catch (error) {
-            res.status(400).json({message: "error has data was not sent poi",error: error})
-        }
-    },
+	createPOI: async (req, res) => {
+		try {
+			req.body.hashtags = req.body.hashtags.split(',');
+			let newPoi = req.body;;
+			await prisma.poi.create({
+				data: newPoi
+			})
+			res.status(200).json({ message: 'Success, a new POI was created: ', data: newPoi });
+		} catch (error) {
+			res.status(400).json({ message: 'Error, POI was not created: ', error: error });
+		}
+	},
 
-   getAllPOI: async (req, res) =>{
-    try {
+  	getAllPOI: async (req, res) =>{
+		try {
+			const result = await prisma.poi.findMany();
+			console.log('result: ');
+			console.log(result);
+			res.status(200).json({ message: 'Success, here is your data: ' , data: result });
+		} catch (error) {
+			res.status(400).json({message: 'Error, POI was not created: ', error: error });
+		}
+	},
 
-    const ret = await prisma.poi.findMany()
+	searchPOI: async (req, res) =>{
+		try {
+			//console.log('req:');
+			//console.log(req);
+			const hashtags = req.body.hashtag;
+			const result = await prisma.poi.findMany({
+				where: {
+						title: 'Folly Beach1',
+					},
+				})
+				console.log('result: ');
+				console.log(result);
+				res.status(200).json({ message: 'Success, here is your data: ' , data: ret });
+			} catch (error) {
+				console.log('req:');
+				console.log(req);
+			res.status(400).json({message: 'Error, POI was not created: ', error: error });
+		}
+  	},
 
-    res.status(200).json({message: "here is your data",data:ret})
-
-    } catch (error) {
-      res.status(400).json(error)
-    }
-
-   },
-   getIndividualPOI: async (req, res) =>{
-    try {
-      const user = await prisma.poi.findUnique({
-        where: {
-          username: req.body.id
-        }
-      })
-  
-    res.status(401).json({message:"invalid password or userid",Error: error})
-   
-    } catch (error) {
-      res.status(400).json({message:"invalide request",Error: error})
-    }
-   }
+  	getIndividualPOI: async (req, res) =>{
+		try {
+			const user = await prisma.poi.findUnique({
+				where: {
+					username: req.body.id
+				}
+			})
+			res.status(401).json({message:"invalid password or userid",Error: error})
+	
+		} catch (error) {
+			res.status(400).json({message:"invalide request",Error: error})
+		}
+	},
     
-  }
-
+}
