c7e81a565052e88e3ed4d0f13990983e7d3d5b4d
Ralph Sabatino <pHerdProductions@gmail.com>
2023-10-06 09:23:30 -0400

    Added /search route

--- server/router/index.js
+++ server/router/index.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 
-const { createPOI, getAllPOI, getIndividualPOI } = require('../controller/poiController');
+const { createPOI, getAllPOI, getIndividualPOI, searchPOI } = require('../controller/poiController');
 const { createUser, getAllUser, loginUser } = require('../controller/userController');
 const { createPost, getAllPost } = require('../controller/postController');
 
 router.route('/signup').post(createUser);
 router.route('/login').post(loginUser);
 router.route('/poi').get(getAllPOI).post(createPOI);
+router.route('/search').get(searchPOI);
 //router.route("/poi/id").get(getIndividualPOI)
 
 router.route('/post').get(getAllPost).post(createPost);
