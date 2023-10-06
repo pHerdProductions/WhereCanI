5e657c64cfc1754a786b217fe33ce4ea093bd6be
Chiemeka Anunkor <chiemeka.anunkor@perficient.com>
2023-10-06 11:18:01 -0500

    apinext for poi

--- client/app/map.jsx
+++ client/app/map.jsx
@@ -1,17 +1,27 @@
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Image, StyleSheet, Text, View, Dimensions, SafeAreaView, Button } from 'react-native';
+import { Image, StyleSheet, Text, View, Dimensions, SafeAreaView, Button, TouchableOpacity, Alert } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import Geocoder from 'react-native-geocoding';
+
+import { GOOGLE_API } from '@env'
+console.log(GOOGLE_API)
+Geocoder.init(GOOGLE_API);
+
 
 export default MapPage = ({ route, navigation }) => {
 
     const deviceWidth = Dimensions.get('screen').width;
     const deviceHeight = Dimensions.get('screen').height;
     const deviceRatio = deviceWidth / deviceHeight; // Device screen's aspect ratio
+    const [marker, setMarker]=useState([])
+    const [counter, setCounter] = useState(0);
+    const [address, setAddress] = useState('');
+
 
     // const { lat, lng, latDelta } = route.params;
-    console.log(route.params)
+    // console.log(route.params)
     // const [region, setRegion] = useState({
     //     latitude: lat,
     //     longitude: lng,
@@ -27,20 +37,34 @@ export default MapPage = ({ route, navigation }) => {
         longitudeDelta: 0.01,
       };
 
-      this.state={
-        markers: []
-      }
-      // this.handlePress=this.handlePress.bind(this)
+        function onPress(e){
+          let area={
+            latitude: e.nativeEvent.coordinate.latitude,
+            longitude: e.nativeEvent.coordinate.longitude,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01,
+          }
+          
+         
+        setMarker([...marker,<Marker draggable={true} key={counter} anchor={{point:(0,3)}} coordinate={area} image={require('./images/WCImark84.png')}
+          onPress={(e)=>{onPoiPress(e)}}
+          >
+          <Text>{address}</Text>            
+          </Marker>
+        ])
+        setCounter((counter) => counter + 1)
+        }
 
-      function handlePress(e){
-        this.setState({
-          markers:[
-            ...this.state.markers,{
-              coordinate: e.nativeEvent.coordinate
-            }
-          ]
-        })
-      }
+        function onPoiPress(e){
+      //     Geocoder.from(e.nativeEvent.coordinate.latitude, e.nativeEvent.coordinate.longitude)
+      //     .then(json => {
+      //     var addressComponent = json.results[0].formatted_address;
+      //     setAddress((address)=>address=addressComponent)
+      // })
+      // .catch(error => console.warn(error));
+      setAddress("map")
+
+        }
 
 
     return (
@@ -51,28 +75,14 @@ export default MapPage = ({ route, navigation }) => {
           <MapView
             style={styles.map}
             initialRegion={regionFollyBeach} // Specify our initial coordinates/region
-            onPress={this.handlePress}
+            onPress={(e)=>{ onPress(e)}}
             // When the user stops scrolling/panning, set the region: 
             // onRegionChangeComplete = {(regionFollyBeach) => setRegion(regionFollyBeach)}
           >
-    
-            {/* Test Marker for Folly Beach around the Washout*/}
-            {/* <Marker draggable={true} anchor={{point:(0,3)}} coordinate={regionFollyBeach} image={require('./images/WCImark84.png')}
-              // onRegionChangeComplete = {(anchor) => anchor (regionFollyBeach)}
-              // onPress={console.log(point)}
-
-            > */}
-
-              {/* <View>
-                <Text style={styles.markerText}>Folly</Text>
-              </View>
-            </Marker> */}
-
-{
-              this.state.markers.map((marker)=>{
-                return <Marker {...marker} image={require('./images/WCImark84.png')}/>
-              })
-            }
+                {
+                  marker
+                }
+        
           </MapView>
           
           
