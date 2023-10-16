9a58b20b4347a240d066e45bcacb46f0bc2ef8f3
pHerdProductions <pherdproductions@gmail.com>
2023-10-16 11:40:15 -0400

    Created a custom-marker reusable component

---
+++ client/components/custom-marker.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { Callout, Marker } from 'react-native-maps';
+
+const generatePOICoordinate = (POI) => {
+	const coordinate = {
+		latitude: parseFloat(POI.latitude),
+		longitude: parseFloat(POI.longitude),
+		latitudeDelta: 0.01,
+		longitudeDelta: 0.01,
+	};
+	return coordinate;
+};
+
+const CustomMarker = React.forwardRef((props, ref) => {
+	let POI = props.POI;
+	return (
+		<Marker
+			coordinate={generatePOICoordinate(POI)}
+			image={require('./../app/images/WCImark84.png')}
+			ref={ref}
+		>
+			<Callout>
+				<View style={{ flex: 1 }}>
+					<Text style={{ color: '#000000', fontWeight: 'bold', fontSize: 15, textAlign: 'center', flex: 1 }}>{POI.title}</Text>
+					<Text style={{ color: '#50006D', fontWeight: 'normal', fontSize: 12, textAlign: 'center', flex: 1 }}>{POI.description}</Text>
+					<Text style={{ color: '#0000C2', fontWeight: 'normal', fontSize: 11, textAlign: 'center', flex: 1 }}>{'#' + POI.hashtags.join(' #')}</Text>
+					<Text style={{ color: '#0F8A0F', fontWeight: 'normal', fontSize: 13, textAlign: 'center', flex: 1, paddingTop: 10 }}>Click Here To Save</Text>
+					<Text style={{ color: '#A80A0A', fontWeight: 'normal', fontSize: 13, textAlign: 'center', flex: 1 }}>Click Off To Delete</Text>
+				</View>
+			</Callout>
+		</Marker>
+	);
+});
+
+export { CustomMarker };
