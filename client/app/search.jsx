de1798b4c5bc3b0ab290209070a4a30ee05d85b6
Ralph Sabatino <pHerdProductions@gmail.com>
2023-12-02 17:28:21 -0500

    Profile button & cleanup

--- client/app/search.jsx
+++ client/app/search.jsx
@@ -1,7 +1,7 @@
 // The Search page, comes after Login / Signup page
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { ThemeProvider, createTheme, Button, Text, Icon } from '@rneui/themed';
-import { View, ScrollView, Keyboard, Alert, TouchableOpacity, Image, KeyboardAvoidingView } from 'react-native';
+import { View, ScrollView, Keyboard, Alert, KeyboardAvoidingView } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { USStates } from '../data/states';
 import * as Search from '../components/search-inputs';
@@ -122,19 +122,6 @@ export default SearchPage = ({ navigation, route }) => {
 		}
 	};
 
-	useEffect(() => {
-		//displaysetting;
-		console.log(route.params);
-	});
-
-	function userDisplay() {
-		if (displaysetting == 'none') {
-			setDisplaysetting('block');
-		} else {
-			setDisplaysetting('none');
-		}
-	}
-
 	return (
 		<SafeAreaProvider style={{ flex: 1, backgroundColor: '#17001F' }}>
 			<KeyboardAvoidingView
@@ -146,53 +133,16 @@ export default SearchPage = ({ navigation, route }) => {
 				<ScrollView>
 					<ThemeProvider theme={theme}>
 						<View style={{ width: '100%', height: '100%', backgroundColor: '#17001F' }}>
-							{/*<Text>User: {JSON.stringify(display)}</Text>*/}
-
-							<View style={{ display: 'flex', flexDirection: 'row-reverse', justifyContent: 'space-between' }}>
-								<TouchableOpacity
-									onPress={() => {
-										//userDisplay();
-										navigation.navigate('profile', { email, username, state, display, id });
-									}}
-									style={{
-										borderRadius: 100,
-										padding: 20,
-										margin: 5,
-										backgroundColor: '#8F00FF',
-										borderColor: '#D49DFF',
-										borderWidth: 1.5,
-										width: 65,
-										height: 65,
-									}}
-								>
-									<Text>{buttonDisplayName}</Text>
-								</TouchableOpacity>
-
-								<View style={{ display: displaysetting, backgroundColor: '17001F', borderBottomWidth: 20 }}>
-									<Image
-										style={{ width: 55, height: 55 }}
-										source={{
-											uri: 'https://cdn-icons-png.flaticon.com/128/3135/3135715.png',
-										}}
-									/>
-									<Text style={{ color: 'white', fontWeight: 'bold' }}>
-										{/* <Icon name='face' /> */}
-										User:{display}
-									</Text>
-									<Text style={{ color: 'white', fontWeight: 'bold' }}>
-										{/* <Icon name='person' /> */}
-										Username:{username}
-									</Text>
-									<Text style={{ color: 'white', fontWeight: 'bold' }}>
-										{/* <Icon name='mail' /> */}
-										Email:{email}
-									</Text>
-									<Text style={{ color: 'white', fontWeight: 'bold' }}>
-										{/* <Icon name='place' /> */}
-										State:{state}
-									</Text>
-								</View>
-							</View>
+							<Button
+								title='Profile'
+								type='outline'
+								raised
+								titleStyle={{ fontSize: 20 }}
+								containerStyle={{ marginTop: '5%', marginLeft: '70%', marginRight: '5%' }}
+								onPress={() => {
+									navigation.navigate('profile', { email, username, state, display, id });
+								}}
+							/>
 							<Text h1>Where Can I...</Text>
 							<Search.StateDropDown
 								ref={(input) => (stateInput = input)}
