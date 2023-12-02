c41e48f96dcbaab854c6d05c4a2cf14f4f3c27b4
Ralph Sabatino <pHerdProductions@gmail.com>
2023-12-02 16:22:41 -0500

    DB/prisma hotfix

---
+++ server/prisma/migrations/20231202212220_remove_rating_avg/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `ratingAvg` on the `Poi` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Poi" DROP COLUMN "ratingAvg";
