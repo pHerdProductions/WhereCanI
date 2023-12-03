4fd7f96ce532c4d1f2a04b013241e99008d88721
Chiemeka Anunkor <chiemeka.anunkor@perficient.com>
2023-12-03 06:49:04 -0600

    create rating

--- server/controller/poiController.js
+++ server/controller/poiController.js
@@ -15,18 +15,13 @@ module.exports = {
 
 	getAllPOI: async (req, res) => {
 		try {
-			// const result = await prisma.poi.findMany({});
+			const result = await prisma.poi.findMany({});
 			// const result = await prisma.poi.findMany({
 			// 	include: {
 			// 		ratings: true,
 			// 	},
 			// });
 
-			const result = await prisma.poi.findFirst({
-				include: {
-					ratings: true,
-				},
-			});
 			console.log('result: ');
 			console.log(result);
 			res.status(200).json({ message: 'Success, here is your data: ', data: result });
