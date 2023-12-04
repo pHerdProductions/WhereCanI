936e32450ba5155320d0a523aea0ea0dc1d7139c
pHerdProductions <pherdproductions@gmail.com>
2023-12-04 16:30:23 -0500

    Callout onPress() goes to PoiPage

--- client/components/custom-marker.jsx
+++ client/components/custom-marker.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { Callout, Marker } from 'react-native-maps';
 import { Rating, RatingProps } from '@rneui/themed';
+import { useNavigation } from '@react-navigation/native';
 
 const generatePOICoordinate = (POI) => {
 	const coordinate = {
@@ -19,27 +20,24 @@ const CustomMarker = React.forwardRef((props, ref) => {
 		console.log('Rating is: ' + rating);
 	};
 
+	const navigation = useNavigation();
+
+	const openPOIPage = (POI) => {
+		navigation.navigate('poi', { POI: POI });
+		console.log(POI);
+	};
+
 	return (
 		<Marker
 			coordinate={generatePOICoordinate(POI)}
 			image={require('./../app/images/WCImark84.png')}
 			ref={ref}
 		>
-			<Callout>
+			<Callout onPress={() => openPOIPage(POI)}>
 				<View style={{ flex: 1 }}>
 					<Text style={{ color: '#000000', fontWeight: 'bold', fontSize: 15, textAlign: 'center', flex: 1 }}>{POI.title}</Text>
 					<Text style={{ color: '#50006D', fontWeight: 'normal', fontSize: 12, textAlign: 'center', flex: 1 }}>{POI.description}</Text>
 					<Text style={{ color: '#0000C2', fontWeight: 'normal', fontSize: 11, textAlign: 'center', flex: 1 }}>{'#' + POI.hashtags.join(' #')}</Text>
-					<Rating
-						style={{ color: '#0000C2', fontWeight: 'normal', fontSize: 11, textAlign: 'center', flex: 1, paddingVertical: 10, width: 5 }}
-						showRating
-						type='star'
-						fractions={1}
-						startingValue={3.6}
-						readonly
-						imageSize={5}
-						onFinishRating={ratingCompleted}
-					/>
 				</View>
 			</Callout>
 		</Marker>
